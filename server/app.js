6c626ca1d81896109d64429f1effd4b7406acd93
Bashar Shoumali <basharshomaly1@hotmail.com>
2025-09-10 14:02:35 +0300

    server update

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json({ limit: "256kb" }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://basharshoumali.github.io"],
     credentials: false,
   })
 );
